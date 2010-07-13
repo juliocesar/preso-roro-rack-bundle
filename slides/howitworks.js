b34eb5eb0d621562ba2eebb890997b735ae9c6e1
Julio Cesar Ody <julioody@gmail.com>
2010-07-13 23:11:15 +1000

    heroku deploy

--- slides/howitworks.js
+++ slides/howitworks.js
@@ -1,3 +1,3 @@
-4,    $('p:gt(1)').oneAtEachTime('animate', [{marginTop: "-1em", opacity: "0"}], 100)
-5.5,  $('p:first').fadeIn(100)
-7,  $('p.hidden:last').fadeIn(100)
+2,    $('p:gt(1)').oneAtEachTime('animate', [{marginTop: "-1em", opacity: "0"}], 100)
+3.5,  $('p:first').fadeIn(100)
+5,  $('p.hidden:last').fadeIn(100)
